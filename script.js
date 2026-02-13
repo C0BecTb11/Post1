a6027fad9ccb76c06dcb31f6b1d46d3014145c73
C0BecTb11 <xaebas120@gmail.com>
2026-02-13 02:34:31 +0100

    Update script.js

--- script.js
+++ script.js
@@ -14,3 +14,31 @@ buttons.forEach(btn => {
       });
   });
 });
+
+document.addEventListener("click", function (e) {
+
+  const btn = e.target.closest(".sub-faq-button");
+  if (!btn) return;
+
+  const faqContainer = document.getElementById("sub-faq-container");
+  if (!faqContainer) return;
+
+  const target = btn.dataset.target;
+
+  fetch(`sections/faq-${target}.html`)
+    .then(res => {
+      if (!res.ok) {
+        faqContainer.innerHTML = `<p>Контент пока не готов.</p>`;
+        return;
+      }
+      return res.text();
+    })
+    .then(html => {
+      if (html) faqContainer.innerHTML = html;
+    })
+    .catch(err => {
+      faqContainer.innerHTML = `<p>Ошибка загрузки подраздела.</p>`;
+      console.error(err);
+    });
+
+});
