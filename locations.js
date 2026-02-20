0dc0d3502363777b6d9723391bc24ef424a046fe
C0BecTb11 <xaebas120@gmail.com>
2026-02-20 01:45:22 +0100

    Create locations.js

---
+++ locations.js
@@ -0,0 +1,20 @@
+// locations.js
+
+const LOCATIONS = [
+  {
+    id: "bunker",
+    title: "Гигантский Бункер",
+    owner: "<span class='faction-link'>Название фракции</span>",
+    img: "images/bunker.jpg",
+    coords: { x: 910, y: 440 },
+    size: 15, // px, диаметр
+    description: `
+      <p>
+        Это массивное подземное сооружение, построенное для защиты...
+        Здесь можно добавить <span class="history-link">историческую ссылку</span>.
+      </p>
+    `
+  }
+
+  // В будущем можно добавлять новые локации сюда
+];
